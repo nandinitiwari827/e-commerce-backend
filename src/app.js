135a71415792774ee5fd90cba067ab1e3dccb58d
nandinitiwari827 <nandini.tiwari1@s.amity.edu>
2026-01-31 19:25:13 +0530

    again cors error fixed

--- src/app.js
+++ src/app.js
@@ -4,27 +4,18 @@ import cookieParser from "cookie-parser"
 
 let app=express()
 
-let allowedOrigins = [
-  "http://localhost:5173",
-  "http://localhost:5174",
-  "https://e-commerce-website-self-kappa.vercel.app"
-];
-
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
+  origin: [
+    "http://localhost:5173",
+    "http://localhost:5174",
+    "https://e-commerce-website-self-kappa.vercel.app"
+  ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
 }));
 
 app.options("*", cors());
-
 
 app.use(express.json({limit: "500mb"}))
 app.use(express.urlencoded({extended: true, limit: "500mb"}))
